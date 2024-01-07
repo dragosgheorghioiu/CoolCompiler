9826d9b992bea37d0e7cfcfbf99077d0401555a0
Dragos GHEORGHIOIU <dragos.gheorghioiu@stud.acs.upb.ro>
2024-01-07 22:43:40 +0200

    new Object

--- src/cool/structures/ObjectBasicClass.java
+++ src/cool/structures/ObjectBasicClass.java
@@ -1,17 +1,30 @@
 package cool.structures;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
 public class ObjectBasicClass extends IdSymbol {
     public ObjectBasicClass(Scope globalScope, String name) {
         super(name);
         this.type = new ClassSymbol(globalScope, name);
-        var abortMethod = new MethodSymbol(globalScope, "abort");
-        abortMethod.setReturnType(new ClassSymbol(globalScope, "Object"));
-        var type_nameMethod = new MethodSymbol(globalScope, "type_name");
-        type_nameMethod.setReturnType(new ClassSymbol(globalScope, "String"));
-        var copyMethod = new MethodSymbol(globalScope, "copy");
-        copyMethod.setReturnType(new ClassSymbol(globalScope, "SELF_TYPE"));
-        this.type.add(abortMethod);
-        this.type.add(type_nameMethod);
-        this.type.add(copyMethod);
+        HashMap<String, List<String>> methods = new HashMap<>();;
+        methods.put("abort", List.of("Object"));
+        methods.put("type_name", List.of("String"));
+        methods.put("copy", List.of("SELF_TYPE"));
+        addMethod(globalScope, methods);
+    }
+
+    protected void addMethod(Scope globalScope, HashMap<String, List<String>> methods) {
+        for (var method : methods.entrySet()) {
+            var methodSymbol = new MethodSymbol(globalScope, method.getKey());
+            methodSymbol.setReturnType(new ClassSymbol(globalScope, method.getValue().get(0)));
+            for (int i = 1; i < method.getValue().size(); i+=2) {
+                var idSymbol = new IdSymbol(method.getValue().get(i));
+                idSymbol.setType(new ClassSymbol(globalScope, method.getValue().get(i+1)));
+                methodSymbol.getFormals().put(idSymbol.getName(), idSymbol);
+            }
+            this.type.add(methodSymbol);
+        }
     }
 }
